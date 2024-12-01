0bbb95f72ca397d67592434ddd0e9ceaf63cc477
nuri-cloud <nurzadatobokelova@gmail.com>
2024-12-01 21:01:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+//loop , цикл
+// do, do-while 
+
+
+// do (условие)  {
+//     code
+// }
+
+// do{
+//     код
+// }while(условие);
+
+// let x = 5;
+// x++;
+
+
+
+// let name = null;
+// let dog = "person";
+// let userName = name ?? dog;
+// console.log(do
+
+
+// let num = 0;
+// while (num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 10;
+
+// do {
+//     console.log(num);
+//     num--;
+
+// }while (num >= 0); 
+//i j k
+// for (let i = 0; i < 7; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 2; j++){
+
+//         console.log(j);
+//     }
+
+// }
+
+let name1 = "";
+
+for (let i = ""; !i; name = prompt("атын ким") ){
+    console.log(i);
+    
+}
+
+// let name = "";
+
+// while (!name) {
+//   name = prompt("атын ким")
+// }
+
+
